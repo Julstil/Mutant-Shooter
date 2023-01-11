dd16aa97d2b4a19c722d41f4f1fdb72f675345d5
Julstil <edvinnordangard@hotmail.com>
2023-01-11 13:32:42 +0100

    Failed door animation
    Frågar Tobias när han är tillbaka

---
+++ Assets/Scripts/c# Edvin/computerinteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class computerinteraction : MonoBehaviour
+{
+    [Header("Player variables")]
+    public Camera playerCamera;
+    public float distanceFromComputer = 2;
+    public string cumputerTag;
+    RaycastHit hitComputer;
+
+    [Header("UI variables")]
+    public Text pressButtenText;
+    public Image img;
+    public KeyCode openDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressButtenText.text = "press " + openDoor + " to open the door";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitComputer, distanceFromComputer))
+        {
+            if (hitComputer.transform.tag == cumputerTag)
+            {
+                img.enabled = true;
+                pressButtenText.enabled = true;
+                if (Input.GetKeyDown(openDoor))
+                {
+                    GetComponent<door>().doorsOpen = true;
+                }
+            }
+            else
+            {
+                img.enabled = false;
+                pressButtenText.enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/c# Edvin/door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class door : MonoBehaviour
+{
+    [HideInInspector]
+    public bool doorsOpen = false;
+    public Animator LeftDoor;
+    public Animator RightDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorsOpen)
+        {
+            LeftDoor.enabled = true;
+            RightDoor.enabled = true;
+            doorsOpen = false;
+        }
+        
+    }
+}
