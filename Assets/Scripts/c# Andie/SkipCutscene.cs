509ee662d8526db213e3019a74edbc8d5804bfea
AndyPandyA <andreas.larsson2@elev.ga.lbs.se>
2023-01-16 11:01:42 +0100

    CutScene

---
+++ Assets/Scripts/c# Andie/SkipCutscene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipCutscene : MonoBehaviour
+{
+    private int sceneIndex;
+
+    private void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(sceneIndex++);
+        }
+    }
+}
