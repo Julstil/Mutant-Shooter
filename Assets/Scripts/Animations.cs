5ce0cd1e193b0d34f39549d8d0da2c02a50020cc
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2023-01-17 11:09:05 +0100

    Hudhsj

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Animations : MonoBehaviour
 {
-    Animation[] animationList;
-    public int currentPos;
+    public Animator[] animatorList;
+    public int currentAnim;
+
+    
     void Start()
     {
         
@@ -14,6 +16,22 @@ public class Animations : MonoBehaviour
     
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            currentAnim++;
+            animatorList[0].SetBool("Press", true);
+            animatorList[0].SetBool("Idle", false);
 
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //animatorList[0].SetBool("Press", true);
+            animatorList[currentAnim].SetBool("Idle", true);
+        }
+    }
+
+    public void LoopEnd()
+    {
+        //animatorList[0].set
     }
 }
