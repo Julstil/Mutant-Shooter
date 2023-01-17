2fd3f89ba5e1fae9d7acb7d117f33f3d3447ead3
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2023-01-17 10:07:27 +0100

    Animation script

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    Animation[] animationList;
+    public int currentPos;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
