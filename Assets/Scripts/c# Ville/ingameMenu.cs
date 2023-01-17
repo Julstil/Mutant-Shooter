3bdf5a8a13a2015c1c87490e1b438f4a6ef24aa8
Vila04 <ville.laurila@elev.ga.lbs.se>
2023-01-17 14:42:05 +0100

    ingameOptions done

---
+++ Assets/Scripts/c# Ville/ingameMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ingameMenu : MonoBehaviour
+{
+    public GameObject creditsPanel;
+    public GameObject quitPanel;
+    public GameObject settingsPanel;
+    public GameObject ingameMenuPanel;
+    void Start()
+    {
+        quitPanel.SetActive(false); //quit-panelen är avstängd i början av scenen - Ville
+        creditsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        ingameMenuPanel.SetActive(false);
+    }
+
+    public void ReturnToGame()
+    {
+        ingameMenuPanel.SetActive(false);
+    }
+    public void Credits() // Öppnar credits - Ville
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CreditsBack() // Döljer credits igen - Ville
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void Settings() // Öppnar settings - Ville
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void SettingsBack() // Döljer settings igen - Ville
+    {
+        settingsPanel.SetActive(false);
+    }
+    public void QuitCheck()   // visar en panel som frågar om du vill avsluta - Ville
+    {
+        quitPanel.SetActive(true);
+    }
+
+    public void QuitBack()  // Går tillbaka till menyn från quit-panelen - Ville
+    {
+        quitPanel.SetActive(false);
+    }
+
+    public void Quit() // 
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ingameMenuPanel.SetActive(true);
+        }
+    }
+}
